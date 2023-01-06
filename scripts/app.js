db7999282844e892b4454dd83ecede6509ea2bb8
Debbie Jones <debbiejones579@aol.com>
2023-01-06 14:18:58 -0700

    Assignment 1

---
+++ scripts/app.js
@@ -0,0 +1,14 @@
+const date = new Date();
+const options = {
+      weekday: "long",
+      month: "long",
+      day: "numeric",
+      year: "numeric",
+    };
+document.querySelector(
+       "#currentdate"
+     ).innerHTML = new Date().toLocaleDateString("en-us", options);
+
+document.querySelector("#currentyear").innerHTML = date.getFullYear();
+
+document.querySelector("#thisupdate").innerHTML = document.lastModified;
