abf296999984a9eb7cb19e261e2b4358ab7e68ec
Debbie Jones <debbiejones579@aol.com>
2023-02-01 10:53:13 -0700

    lesson3

---
+++ lesson5/scripts/app.js
@@ -0,0 +1,18 @@
+
+
+
+const button =document.querySelector('button');
+
+button.addEventListener('click', () => {
+    if (input.value !--)
+})
+
+const input = document.querySelector('input')
+
+input.addEventListener('click', function() {})
+
+const list = document.querySelector('ul')
+
+input.addEventListener('click', function(){})
+
+const deleteBtn = document.createElement('li')
